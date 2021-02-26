65af3bc099ec69dfb182d6355a93c46ebeb88eed
d.paz.castro <d.paz.castro@accenture.com>
2021-02-26 21:26:17 +0100

    twitch chatbot connection after connecting client

--- server.js
+++ server.js
@@ -1,53 +1,18 @@
-const net = require("net");
+const io = require("socket.io");
 
-// const createServer = () => {
-//   const server = net
-//     .createServer((socket) => {
-//       socket.on("data", (data) => {
-//         console.log(data.toString);
-//       });
-//     })
-//     .on("error", (err) => {
-//       console.log(err);
-//     });
+const createServer = (cb) => {
+  const httpServer = require("http").createServer();
+  const socketServer = io(httpServer);
 
-//   server.listen(3000, () => {
-//     console.log("Opened server on", server.address().port);
-//   });
-//   return server;
-// };
-
-// const createServer = () => {
-//   const io = require("socket.io")();
-//   io.on("connection", (client) => {
-//     client.on("event", (data) => {
-//       console.log("client.on-event", data);
-//     });
-//     client.on("disconnect", () => {
-//       console.log("client.on-disconnect", data);
-//     });
-//   });
-//   io.listen(3000);
-// };
-
-const createServer = () => {
-  const httpServer = require("http").createServer((req, res) => {
-    // serve the index.html file
-    res.setHeader("Content-Type", "text/html");
-  });
-
-  const io = require("socket.io")(httpServer);
-
-  io.on("connection", (socket) => {
+  socketServer.on("connection", (socket) => {
     console.log("connect");
-    socket.on('hey', data => {
-      console.log('hey', data);
+    socket.onAny((data) => {
+      console.log("hey", data);
     });
+    cb(socket);
   });
-
-  httpServer.listen(3000, () => {
-    console.log("go to http://localhost:3000");
-  });
+  socketServer.on("error", console.log);
+  httpServer.listen(3000);
 };
 
 // module.exports = createServer;
